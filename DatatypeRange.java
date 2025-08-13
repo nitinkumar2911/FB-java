0f7a79c7d7783b18b09085d81f81d56c5fbfb59f
NitinKumar1054 <nitin2911.in@gmail.com>
2025-08-13 13:55:49 +0530

    Add files via upload

---
+++ DatatypeRange.java
@@ -0,0 +1,12 @@
+public class DatatypeRange {
+    public static void main(String[] args){
+        System.out.println("Range from "+Byte.MIN_VALUE +" to "+ Byte.MAX_VALUE);
+        System.out.println("Range from "+(int)Character.MIN_VALUE +" to "+ (int)Character.MAX_VALUE);
+        System.out.println("Range from "+Integer.MIN_VALUE +" to "+ Integer.MAX_VALUE);
+        System.out.println("Range from "+Short.MIN_VALUE +" to "+ Short.MAX_VALUE);
+        System.out.println("Range from "+Long.MIN_VALUE +" to "+ Long.MAX_VALUE);
+        System.out.println("Range from "+Float.MIN_VALUE +" to "+ Float.MAX_VALUE);
+        System.out.println("Range from "+Double.MIN_VALUE +" to "+ Double.MAX_VALUE);
+    } 
+    
+}

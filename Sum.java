5d0d7d01382699dffaad4dde61641c8933a422e7
NitinKumar1054 <nitin2911.in@gmail.com>
2025-08-13 13:55:27 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Sum {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 67;
+        int sum = a+b;
+        System.out.println("Sum is : "+ sum);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number1: ");
+        int num1 = sc.nextInt();
+        System.out.println("Enter the number2 : ");
+        int num2 = sc.nextInt();
+        System.out.println(num1 + num2);
+        
+    }
+    
+}

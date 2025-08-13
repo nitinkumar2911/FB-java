3fc1235e3a9564b9c4ab813c0fc00bf1e87f7064
NitinKumar1054 <nitin2911.in@gmail.com>
2025-08-13 10:07:51 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,9 @@
+public class Variable {
+    static int a = 5; // static variable
+    public static void main(String[] args){
+        int b = 6;  //  local variable
+        System.out.println(b);
+        System.out.println(Variable.a);
+    }
+    
+}

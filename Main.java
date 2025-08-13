e7cc2149b94125f4d5fe78e3094a3bddd6fb652c
NitinKumar1054 <nitin2911.in@gmail.com>
2025-08-13 09:24:01 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("Nitin Kumar");
+
+        System.out.println("B-Tech");
+        System.out.println("2415001054");
+
+    }
+    
+}

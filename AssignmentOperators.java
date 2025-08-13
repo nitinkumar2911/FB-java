87bfc2355d7d3b4ed1482a6b126add31f21faa0e
NitinKumar1054 <nitin2911.in@gmail.com>
2025-08-13 14:47:06 +0530

    Add files via upload

---
+++ AssignmentOperators.java
@@ -0,0 +1,13 @@
+public class AssignmentOperators {
+    public static void main(String[] args){
+        int a = 5;
+         System.out.println( a);
+        System.out.println( a+=2);
+        System.out.println( a-=2);
+        System.out.println( a*=2);
+        System.out.println( a/=2);
+       
+
+    }
+    
+}

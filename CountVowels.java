5fcec502818d270c6114c16606cb588162d680a4
NitinKumar1054 <nitin2911.in@gmail.com>
2025-10-13 09:17:31 +0530

    Count Vowels java solution

---
+++ CountVowels.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class CountVowels {
+    public static void main(String[] args){
+        Scanner sc  = new Scanner(System.in);
+        String s = sc.nextLine();
+        s = s.toLowerCase();
+        int c =0;
+        for(int i = 0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o'|| ch == 'u' ){
+                c++;
+
+            }
+        }
+        System.out.println("Vowels are : "+ c);
+    }
+    
+}
